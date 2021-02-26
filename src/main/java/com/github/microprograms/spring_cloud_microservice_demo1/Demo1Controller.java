fc245a63160b55b77aca27e7b892eb07c11369f3
xuzewei <microprograms@126.com>
2021-02-26 15:35:01 +0800

    openfeign

--- src/main/java/com/github/microprograms/spring_cloud_microservice_demo1/Demo1Controller.java
+++ src/main/java/com/github/microprograms/spring_cloud_microservice_demo1/Demo1Controller.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Demo1Controller {
 
-    @RequestMapping("/health")
+    @RequestMapping(value = "/health", produces = "text/plain;charset=UTF-8")
     public String health() {
-        return "Ok";
+        return "Ok!";
     }
 
 }
--- src/main/java/com/github/microprograms/spring_cloud_microservice_demo1/SpringCloudMicroserviceDemo1Application.java
+++ src/main/java/com/github/microprograms/spring_cloud_microservice_demo1/SpringCloudMicroserviceDemo1Application.java
@@ -2,10 +2,10 @@ package com.github.microprograms.spring_cloud_microservice_demo1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class SpringCloudMicroserviceDemo1Application {
 
 	public static void main(String[] args) {
