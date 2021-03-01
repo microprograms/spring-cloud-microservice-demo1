2367b911b7f7eaeb0f57c3bcda66820e5d8b7fa6
xuzewei <microprograms@126.com>
2021-03-01 16:36:26 +0800

    Thread.sleep(5 * 1000);

--- src/main/java/com/github/microprograms/spring_cloud_microservice_demo1/Demo1Controller.java
+++ src/main/java/com/github/microprograms/spring_cloud_microservice_demo1/Demo1Controller.java
@@ -1,5 +1,7 @@
 package com.github.microprograms.spring_cloud_microservice_demo1;
 
+import java.util.Random;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class Demo1Controller {
 
     @GetMapping("/health")
-    public String health() {
+    public String health() throws InterruptedException {
+        if (new Random().nextBoolean()) {
+            Thread.sleep(5 * 1000);
+        }
         return "Ok!";
     }
 
