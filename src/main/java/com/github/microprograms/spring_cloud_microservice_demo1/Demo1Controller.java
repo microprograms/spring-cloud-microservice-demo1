612435cb2c9c045e2e8bf535f0a6876854bdf3fe
xuzewei <microprograms@126.com>
2021-03-01 18:06:32 +0800

    随机端口

--- src/main/java/com/github/microprograms/spring_cloud_microservice_demo1/Demo1Controller.java
+++ src/main/java/com/github/microprograms/spring_cloud_microservice_demo1/Demo1Controller.java
@@ -2,18 +2,23 @@ package com.github.microprograms.spring_cloud_microservice_demo1;
 
 import java.util.Random;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Demo1Controller {
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/health")
     public String health() throws InterruptedException {
         if (new Random().nextBoolean()) {
             Thread.sleep(5 * 1000);
         }
-        return "Ok!";
+        return environment.getProperty("local.server.port") + " is Ok!";
     }
 
 }
